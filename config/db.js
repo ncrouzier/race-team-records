26cec2d047e4965384d9bd5b8cdb7025177866b9
Nicolas Crouzier <achille.talon@gmail.com>
2015-01-23 22:43:47 -0500

    bower script at build and mongodb update

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url : process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1:27017/records'
+	url : process.env.OPENSHIFT_MONGODB_DB_URL+'/records' || 'mongodb://127.0.0.1:27017/records'
 }

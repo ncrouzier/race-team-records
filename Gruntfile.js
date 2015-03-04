71965fe4fba0a618ea7be9fa055d61bd8d59beed
Nicolas Crouzier <achille.talon@gmail.com>
2015-03-03 23:35:34 -0500

    better css management started with smarter use of less files

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,7 +46,7 @@ module.exports = function(grunt) {
         less: {
             build: {
                 files: {
-                    'public/dist/css/style.css': 'public/css/*.less'
+                    'public/dist/css/style.css': 'public/less/**/*.less'
                 }
             }
         },
@@ -62,6 +62,7 @@ module.exports = function(grunt) {
                     'public/dist/css/style.min.css': [
                         'public/libs/fontawesome/css/font-awesome.css',
                         'public/css/libs/select2.css',
+                        'public/css/libs/select2-override.css',
                         'public/css/libs/bootstrap.css',
                         'public/libs/angular-loading-bar/build/loading-bar.css',
                         'public/libs/angular-ui-select/dist/select.css',
@@ -101,7 +102,7 @@ module.exports = function(grunt) {
         // watch css and js files and process the above tasks
         watch: {
             css: {
-                files: ['public/css/**/*.less', 'public/css/**/*.css'],
+                files: ['public/less/**/*.less', 'public/css/**/*.css'],
                 tasks: ['less', 'cssmin']
             },
             js: {

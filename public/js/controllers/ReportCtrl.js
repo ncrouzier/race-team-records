51d6aed2e90fff115f242bc4231f8ebd1c2064cb
Nicolas Crouzier <achille.talon@gmail.com>
2016-04-11 14:51:22 -0400

    report fix

--- public/js/controllers/ReportCtrl.js
+++ public/js/controllers/ReportCtrl.js
@@ -23,7 +23,7 @@ angular.module('mcrrcApp.results').controller('ReportController', ['$scope', '$s
                 "datefrom": $filter('date')($scope.datefrom, "yyyy-MM-dd", 'UTC'),
                 "dateto": $filter('date')($scope.dateto, "yyyy-MM-dd", 'UTC')
             },
-            "sort": '-racedate racename time'
+            "sort": '-race.racedate race.racename time'
         }).then(function(results) {
             $scope.report = "";
             var lastEvent = "";
@@ -34,11 +34,11 @@ angular.module('mcrrcApp.results').controller('ReportController', ['$scope', '$s
                     members += m.firstname + ' ' + m.lastname + ' & ';
                 });
                 members = members.slice(0, -3);
-                if (result.racename !== lastEvent || result.racedate !== lastDate) {
+                if (result.race.racename !== lastEvent || result.race.racedate !== lastDate) {
                     if ($scope.report !== '') {
                         $scope.report += '\n';
                     }
-                    $scope.report += result.racename + " -- " + $filter('date')(result.racedate, "yyyy-MM-dd", 'UTC') + "\n";
+                    $scope.report += result.race.racename + " -- " + $filter('date')(result.race.racedate, "yyyy-MM-dd", 'UTC') + "\n";
                     $scope.report += members + " " + $filter('secondsToTimeString')(result.time);
                     if (result.ranking) {
                         $scope.report += " (" + $filter('rankTooltipOneLine')(result.ranking) + ")";
@@ -52,8 +52,8 @@ angular.module('mcrrcApp.results').controller('ReportController', ['$scope', '$s
                     $scope.report += "\n";
                 }
 
-                lastEvent = result.racename;
-                lastDate = result.racedate;
+                lastEvent = result.race.racename;
+                lastDate = result.race.racedate;
             });
         });
     };
@@ -64,7 +64,7 @@ angular.module('mcrrcApp.results').controller('ReportController', ['$scope', '$s
                 "datefrom": $filter('date')($scope.datefrom, "yyyy-MM-dd", 'UTC'),
                 "dateto": $filter('date')($scope.dateto, "yyyy-MM-dd", 'UTC')
             },
-            "sort": '-racedate racename time'
+            "sort": '-race.racedate race.racename time'
         }).then(function(results) {
             $scope.reportHTLM = '<table style="width:400px;border:1px;border-collapse: collapse;color:#646464;">';
 
@@ -76,19 +76,19 @@ angular.module('mcrrcApp.results').controller('ReportController', ['$scope', '$s
                     members += m.firstname + ' ' + m.lastname + ' & ';
                 });
                 members = members.slice(0, -3);
-                if (result.racename !== lastEvent || result.racedate !== lastDate) {
+                if (result.race.racename !== lastEvent || result.race.racedate !== lastDate) {
                     if ($scope.reportHTLM !== '<table style="width:400px;border:1px;border-collapse: collapse;color:#646464;">') {
                         $scope.reportHTLM += '<tr style="height: 30px;"></tr>';
                     } 
-                    $scope.reportHTLM += '<thead><tr style="color: #FA4D19; font-size: 18px;border-bottom-width: 1px;border-bottom-style: dashed;"><th style="text-align:center;" colspan="5">' + result.racename + ' - ' + $filter('date')(result.racedate, "yyyy-MM-dd", 'UTC') + '</th></tr><tr style="color: #19c6fa; font-weight:bold"><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Racer</th><th style="text-align:center;font-size: 14px;" colspan="3">Finish Place</th><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Time</th></tr><tr style="border-bottom: 1px solid #E6E6E6;" ><th style="text-align:center;font-weight:normal;font-size:12px;">Age</th><th style="text-align:center;font-weight:normal;font-size:12px;">Gender</th><th style="text-align:center;font-weight:normal;font-size:12px;">Overall</th></tr></thead>';
+                    $scope.reportHTLM += '<thead><tr style="color: #FA4D19; font-size: 18px;border-bottom-width: 1px;border-bottom-style: dashed;"><th style="text-align:center;" colspan="5">' + result.race.racename + ' - ' + $filter('date')(result.race.racedate, "yyyy-MM-dd", 'UTC') + '</th></tr><tr style="color: #19c6fa; font-weight:bold"><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Racer</th><th style="text-align:center;font-size: 14px;" colspan="3">Finish Place</th><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Time</th></tr><tr style="border-bottom: 1px solid #E6E6E6;" ><th style="text-align:center;font-weight:normal;font-size:12px;">Age</th><th style="text-align:center;font-weight:normal;font-size:12px;">Gender</th><th style="text-align:center;font-weight:normal;font-size:12px;">Overall</th></tr></thead>';
                     $scope.reportHTLM += '<tr><td style="font-weight:bold;">' + members + '</td>' + $filter('rankTooltipTd')(result.ranking) + '<td style="text-align: center;"><span style="cursor:pointer;" title="pace: ' + $filter('resultToPace')(result) + '">' + $filter('secondsToTimeString')(result.time) + '</span></td></tr>';
                 } else {
 
                     $scope.reportHTLM += '<tr><td style="font-weight:bold;">' + members + '</td>' + $filter('rankTooltipTd')(result.ranking) + '<td style="text-align: center;"><span style="cursor:pointer;" title="pace: ' + $filter('resultToPace')(result) + '">' + $filter('secondsToTimeString')(result.time) + '</span></td></tr>';
                 }
 
-                lastEvent = result.racename;
-                lastDate = result.racedate;
+                lastEvent = result.race.racename;
+                lastDate = result.race.racedate;
             });
             $scope.reportHTLM += '</table><span style="color:#646464;font-size:12px;">See all results from the MCRRC racing team at <a href="http://raceteam.mcrrc.org" target="_blank">raceteam.mcrrc.org</a></span>';
         });
@@ -113,7 +113,7 @@ angular.module('mcrrcApp.results').controller('TableReportController', ['$scope'
                 "datefrom": $filter('date')($stateParams.from, "yyyy-MM-dd", 'UTC'),
                 "dateto": $filter('date')($stateParams.to, "yyyy-MM-dd", 'UTC')
             },
-            "sort": '-racedate racename time'
+            "sort": '-race.racedate race.racename time'
         }).then(function(results) {
             $scope.reportHTLM = '<table style="width:400px;border:1px;border-collapse: collapse;color:#646464;">';
 
@@ -125,11 +125,11 @@ angular.module('mcrrcApp.results').controller('TableReportController', ['$scope'
                     members += m.firstname + ' ' + m.lastname + ' & ';
                 });
                 members = members.slice(0, -3);
-                if (result.racename !== lastEvent || result.racedate !== lastDate) {
+                if (result.race.racename !== lastEvent || result.race.racedate !== lastDate) {
                     if ($scope.reportHTLM !== '<table style="width:400px;border:1px;border-collapse: collapse;color:#646464;">') {
                         $scope.reportHTLM += '<tr style="height: 30px;"></tr>';
                     }
-                    $scope.reportHTLM += '<thead><tr style="color: #FA4D19; font-size: 18px;border-bottom-width: 1px;border-bottom-style: dashed;"><th style="text-align:center;" colspan="5">' + result.racename + ' - ' + $filter('date')(result.racedate, "yyyy-MM-dd", 'UTC') + '</th></tr><tr style="color: #19c6fa; font-weight:bold"><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Racer</th><th style="text-align:center;font-size: 14px;" colspan="3">Finish Place</th><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Time</th></tr><tr style="border-bottom: 1px solid #E6E6E6;" ><th style="text-align:center;font-weight:normal;font-size:12px;">Age</th><th style="text-align:center;font-weight:normal;font-size:12px;">Gender</th><th style="text-align:center;font-weight:normal;font-size:12px;">Overall</th></tr></thead>';
+                    $scope.reportHTLM += '<thead><tr style="color: #FA4D19; font-size: 18px;border-bottom-width: 1px;border-bottom-style: dashed;"><th style="text-align:center;" colspan="5">' + result.race.racename + ' - ' + $filter('date')(result.race.racedate, "yyyy-MM-dd", 'UTC') + '</th></tr><tr style="color: #19c6fa; font-weight:bold"><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Racer</th><th style="text-align:center;font-size: 14px;" colspan="3">Finish Place</th><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Time</th></tr><tr style="border-bottom: 1px solid #E6E6E6;" ><th style="text-align:center;font-weight:normal;font-size:12px;">Age</th><th style="text-align:center;font-weight:normal;font-size:12px;">Gender</th><th style="text-align:center;font-weight:normal;font-size:12px;">Overall</th></tr></thead>';
                     $scope.reportHTLM += '<tr><td style="font-weight:bold;">' + members + '</td>' + $filter('rankTooltipTd')(result.ranking) + '<td style="text-align: center;"><span style="cursor:pointer;" title="pace: ' + $filter('resultToPace')(result) + '">' + $filter('secondsToTimeString')(result.time) + '</span></td></tr>';
 
                 } else {
@@ -138,8 +138,8 @@ angular.module('mcrrcApp.results').controller('TableReportController', ['$scope'
 
                 }
 
-                lastEvent = result.racename;
-                lastDate = result.racedate;
+                lastEvent = result.race.racename;
+                lastDate = result.race.racedate;
             });
             $scope.reportHTLM += '</table><span style="color:#646464;font-size:12px;">See all results from the MCRRC racing team at <a href="http://raceteam.mcrrc.org" target="_blank">raceteam.mcrrc.org</a></span>';
         });

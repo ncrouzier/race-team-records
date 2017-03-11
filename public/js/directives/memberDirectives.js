6c51f025c3d4009969e40f7a0fdfb63ddcf7c9c2
Nicolas Crouzier <achille.talon@gmail.com>
2017-03-11 12:57:29 -0500

    name in result detail page

--- public/js/directives/memberDirectives.js
+++ public/js/directives/memberDirectives.js
@@ -7,3 +7,8 @@ app.directive('resultMembersNames', function() {
     };
 });
 
+app.directive('resultMembersNamesFull', function() {
+    return { 
+        template: ' <a class="hoverhand" ui-sref="/members({ member: result.members[0].firstname+result.members[0].lastname })"><span uib-tooltip="{{result | membersNamesWithAgeFilter}}" >{{result.members | membersNamesFilter}}</span></a>'
+    };
+});

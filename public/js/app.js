c585cda019ccaa2542742a8aaf24be24e69509a3
Nicolas Crouzier <>
2019-08-05 12:45:12 -0400

    Added two new header

--- public/js/app.js
+++ public/js/app.js
@@ -40,8 +40,11 @@ app.factory('MyCachingRestService', function(Restangular) {
 angular.module('mcrrcApp.results').controller('MainController', ['$scope', 'AuthService', '$state', 'ResultsService', function($scope, AuthService, $state, ResultsService) {
     $scope.$state = $state;
 
-    var navBackGround = ["navimg-2", "navimg-3"];
-    var navBackGroundCR = ['<a href="http://www.mcrrcphotos.com/2017-Photos/Race-Photos/Piece-of-Cake-10K5K/Piece-of-Cake-10K5K-BButters/i-5hgMqmQ/A" target="_blank" title="Photo by B.Butters at Piece of Cake 10K 2017">© B.Butters</a>','<a href="https://www.facebook.com/pg/gburgmd/photos/?tab=album&album_id=10154376948800741" target="_blank" title="Photo by the City of Gaithersburg at La Milla de Mayo 2017">© City of Gaithersburg</a>'];
+    var navBackGround = ["navimg-2", "navimg-3","navimg-4","navimg-5"];
+    var navBackGroundCR = ['<a href="http://www.mcrrcphotos.com/2017-Photos/Race-Photos/Piece-of-Cake-10K5K/Piece-of-Cake-10K5K-BButters/i-5hgMqmQ/A" target="_blank" title="Photo by B.Butters at Piece of Cake 10K 2017">© B.Butters</a>',
+    '<a href="https://www.facebook.com/pg/gburgmd/photos/?tab=album&album_id=10154376948800741" target="_blank" title="Photo by the City of Gaithersburg at La Milla de Mayo 2017">© City of Gaithersburg</a>',
+    '<a href="https://www.mcrrcphotos.com/2019-Photos/Race-Photos/Going-Green-Track-Meet-/Going-Green-Track-Meet-DReichmann/i-MVnQgnh/A" target="_blank" title="Photo by D.Reichmann at Going Green Track Meet 2019">© D.Reichmann</a>',
+    '<a href="https://www.mcrrcphotos.com/2018-Photos/Race-Photos/Midsummer-Nights-Mile-DReichmann/i-8rB2WKf/A" target="_blank" title="Photo by D.Reichmann at Midsummer Night\'s Mile 2018">© D.Reichmann</a>'];
     var random = Math.floor(Math.random() * navBackGround.length );
     $scope.getbg = navBackGround[random];
     $scope.getcr = navBackGroundCR[random];

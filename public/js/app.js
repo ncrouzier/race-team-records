21e62714b007d7383547feed1149689046945cd3
Nicolas Crouzier <>
2021-04-14 20:09:57 -0400

    top corner

--- public/js/app.js
+++ public/js/app.js
@@ -40,11 +40,12 @@ app.factory('MyCachingRestService', function(Restangular) {
 angular.module('mcrrcApp.results').controller('MainController', ['$scope', 'AuthService', '$state', 'ResultsService','MembersService', function($scope, AuthService, $state, ResultsService,MembersService) {
     $scope.$state = $state;
 
-    var navBackGround = ["navimg-2", "navimg-3","navimg-4","navimg-5"];
+    var navBackGround = ["navimg-2", "navimg-3","navimg-4","navimg-5","navimg-6" ];
     var navBackGroundCR = ['<a href="http://www.mcrrcphotos.com/2017-Photos/Race-Photos/Piece-of-Cake-10K5K/Piece-of-Cake-10K5K-BButters/i-5hgMqmQ/A" target="_blank" title="Photo by B.Butters at Piece of Cake 10K 2017">© B.Butters</a>',
     '<a href="https://www.facebook.com/pg/gburgmd/photos/?tab=album&album_id=10154376948800741" target="_blank" title="Photo by the City of Gaithersburg at La Milla de Mayo 2017">© City of Gaithersburg</a>',
     '<a href="https://www.mcrrcphotos.com/2019-Photos/Race-Photos/Going-Green-Track-Meet-/Going-Green-Track-Meet-DReichmann/i-MVnQgnh/A" target="_blank" title="Photo by D.Reichmann at Going Green Track Meet 2019">© D.Reichmann</a>',
-    '<a href="https://www.mcrrcphotos.com/2018-Photos/Race-Photos/Midsummer-Nights-Mile-DReichmann/i-8rB2WKf/A" target="_blank" title="Photo by D.Reichmann at Midsummer Night\'s Mile 2018">© D.Reichmann</a>'];
+    '<a href="https://www.mcrrcphotos.com/2018-Photos/Race-Photos/Midsummer-Nights-Mile-DReichmann/i-8rB2WKf/A" target="_blank" title="Photo by D.Reichmann at Midsummer Night\'s Mile 2018">© D.Reichmann</a>',
+    '<a href="https://www.instagram.com/dwhitphoto/" target="_blank" title="Photo by Dustin Whitlow at Army 10 Miler 2018">© Dustin Whitlow</a>'];
     var random = Math.floor(Math.random() * navBackGround.length );
     $scope.getbg = navBackGround[random];
     $scope.getcr = navBackGroundCR[random];

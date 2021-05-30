a486493b1a5ee732fd452265b4f8d761a8b7fb49
Nicolas Crouzier <>
2021-05-30 12:57:09 -0400

    birthday race icon

--- public/js/directives/resultDirectives.js
+++ public/js/directives/resultDirectives.js
@@ -14,10 +14,22 @@ app.directive('resultIcon', function() {
         link: function($scope, element, attrs) {
           // $scope.imgsrc = "";
           // $scope.title = "";
+
+          function sameDay(d1, d2) {
+            return d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
+          }
+
+
           if ($scope.result.customOptions !== undefined){
             $scope.resultIcons = $scope.result.customOptions.filter(x => x.name === "resultIcon");
           }
+          $scope.isbirthdayRace = false;
+          $scope.result.members.forEach(function(member) {
+              if(sameDay(new Date(member.dateofbirth),new Date($scope.result.race.racedate))){
+                  $scope.isbirthdayRace = true;
+              }
+          });
         },
-        template:	'<span ng-if="resultIcons.length >0" ng-repeat="resultIcon in resultIcons track by $index"  class="hoverhand resultIcons" uib-tooltip-html="resultIcon.text"><img ng-src="{{resultIcon.value}}"  width="16" height="16"></span>'
+        template:	'<span class="hoverhand" uib-tooltip="Birthday Race" ng-if="isbirthdayRace">🎂</span> <span ng-if="resultIcons.length >0" ng-repeat="resultIcon in resultIcons track by $index"  class="hoverhand resultIcons" uib-tooltip-html="resultIcon.text"><img ng-src="{{resultIcon.value}}"  width="16" height="16"></span>'
     };
 });

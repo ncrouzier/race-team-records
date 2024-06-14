50b5fbcbcbeacf41372ea37b7650a671a81fc83e
Nicolas Crouzier <achille.talon@gmail.com>
2024-06-14 09:38:35 -0400

    fixed report creation

--- public/js/controllers/ReportCtrl.js
+++ public/js/controllers/ReportCtrl.js
@@ -28,7 +28,7 @@ angular.module('mcrrcApp.results').controller('ReportController', ['$scope', '$s
         }).then(function(raceInfosList) {
             $scope.report = "";
             raceInfosList.forEach(function(raceinfo) {
-                $scope.report += raceinfo.race.racename + " -- " + $filter('date')(raceinfo.race.racedate, "yyyy-MM-dd", 'UTC') + "\n";
+                $scope.report += raceinfo.racename + " -- " + $filter('date')(raceinfo.racedate, "yyyy-MM-dd", 'UTC') + "\n";
                 raceinfo.results.forEach(function(result) {
                     var members = '';
                     result.members.forEach(function(m) {
@@ -60,14 +60,14 @@ angular.module('mcrrcApp.results').controller('ReportController', ['$scope', '$s
         }).then(function(raceInfosList) {
             $scope.reportHTLM = '<table style="width:400px;border:1px;border-collapse: collapse;color:#646464;">';
             raceInfosList.forEach(function(raceinfo) {
-                $scope.reportHTLM += '<thead><tr style="color: #FA4D19; font-size: 18px;border-bottom-width: 1px;border-bottom-style: dashed;"><th style="text-align:center;" colspan="5">' + raceinfo.race.racename + ' - ' + $filter('date')(raceinfo.race.racedate, "yyyy-MM-dd", 'UTC') + '</th></tr><tr style="color: #19c6fa; font-weight:bold"><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Racer</th><th style="text-align:center;font-size: 14px;" colspan="3">Finish Place</th><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Time</th></tr><tr style="border-bottom: 1px solid #E6E6E6;" ><th style="text-align:center;font-weight:normal;font-size:12px;">Age</th><th style="text-align:center;font-weight:normal;font-size:12px;">Gender</th><th style="text-align:center;font-weight:normal;font-size:12px;">Overall</th></tr></thead>';
+                $scope.reportHTLM += '<thead><tr style="color: #FA4D19; font-size: 18px;border-bottom-width: 1px;border-bottom-style: dashed;"><th style="text-align:center;" colspan="5">' + raceinfo.racename + ' - ' + $filter('date')(raceinfo.racedate, "yyyy-MM-dd", 'UTC') + '</th></tr><tr style="color: #19c6fa; font-weight:bold"><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Racer</th><th style="text-align:center;font-size: 14px;" colspan="3">Finish Place</th><th style="text-align:center;border-bottom: 1px solid #E6E6E6;font-size: 16px;" rowspan="2">Time</th></tr><tr style="border-bottom: 1px solid #E6E6E6;" ><th style="text-align:center;font-weight:normal;font-size:12px;">Age</th><th style="text-align:center;font-weight:normal;font-size:12px;">Gender</th><th style="text-align:center;font-weight:normal;font-size:12px;">Overall</th></tr></thead>';
                 raceinfo.results.forEach(function(result) {
                     var members = '';
                     result.members.forEach(function(m) {
                         members += m.firstname + ' ' + m.lastname + ' & ';
                     });
                     members = members.slice(0, -3);
-                    $scope.reportHTLM += '<tr><td style="font-weight:bold;">' + members + '</td>' + $filter('rankTooltipTd')(result.ranking) + '<td style="text-align: center;"><span style="cursor:pointer;" title="pace: ' + $filter('resultToPace')(result,raceinfo.race) + '">' + $filter('secondsToTimeString')(result.time) + '</span></td></tr>';
+                    $scope.reportHTLM += '<tr><td style="font-weight:bold;">' + members + '</td>' + $filter('rankTooltipTd')(result.ranking) + '<td style="text-align: center;"><span style="cursor:pointer;" title="pace: ' + $filter('resultToPace')(result,raceinfo) + '">' + $filter('secondsToTimeString')(result.time) + '</span></td></tr>';
                 });
             });
             $scope.reportHTLM += '</table><span style="color:#646464;font-size:12px;">See all results from the MCRRC racing team at <a href="http://raceteam.mcrrc.org" target="_blank">raceteam.mcrrc.org</a></span>';

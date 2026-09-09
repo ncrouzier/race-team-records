9ee4e99c12a540e3ad42285dfd1c30a5bfd1d87f
Nicolas Crouzier <>
2026-09-08 20:40:00 -0400

    removed unused files

--- config/db.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-        url: process.env.OPENSHIFT_MONGODB_DB_URL + 'records'; // connect to our database
-    } else {
-        url: 'mongodb://127.0.0.1:27017/records'; // connect to our database
-    }
-}
